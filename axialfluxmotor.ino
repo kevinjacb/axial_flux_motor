625e71c7781ee9fba800fb99b230e225149002c8
Kevin Jacob <38008146+kevinjacb@users.noreply.github.com>
2021-10-05 00:06:21 +0530

    Update axialfluxmotor.ino

--- axialfluxmotor.ino
+++ axialfluxmotor.ino
@@ -3,17 +3,16 @@ const int sensor = 9;
 const int output1 = 4, output2 = 5;
 bool state = false;
 void setup() {
-  // put your setup code here, to run once:
-   pinMode(sensor, INPUT);
+   pinMode(sensor, INPUT);  //initializing the respective pins
    pinMode(output1,OUTPUT);
    pinMode(output2,OUTPUT);
    attachInterrupt(digitalPinToInterrupt(sensor),triggered, CHANGE);
 }
 
-void loop() {
+void loop() { //Sets the pole of the coil(s)
     digitalWrite(output1,state);
     digitalWrite(output2,!state);
 }
-void triggered(){
+void triggered(){ //Triggers a change in pole when the condition is met
   state = !state;
 }
